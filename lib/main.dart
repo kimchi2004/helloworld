893a19b7a10c4960f652dbb06fd8574bf23f7eb9
admin <kimchi200401@gmail.com>
2023-03-21 14:24:04 +0700

    Revert "delete name"
    This reverts commit 1b8c3c6358b61ecb8d2f6ae1aeb970c6774b5e9d.

--- lib/main.dart
+++ lib/main.dart
@@ -96,6 +96,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
+              'Ngo Thi Kim Chi'
                   'You have pushed the button this many times:',
             ),
             Text(
