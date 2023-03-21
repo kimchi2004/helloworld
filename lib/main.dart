2fa4b74ee13d70bfc29b2d1db364bf1aee4bd6d3
admin <kimchi200401@gmail.com>
2023-03-21 14:23:18 +0700

    Revert "change tittle"
    This reverts commit 9c8be88dd712039f21c403da565eee929b86fdea.

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Hello World',
+      title: 'Flutter Demo',
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -24,7 +24,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Cheichei'),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
